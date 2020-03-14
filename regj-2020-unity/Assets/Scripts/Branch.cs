cd051aefba879cfc04d62b6db9cc90c598c2c92c
Ethan Moffatt <darker1300@gmail.com>
2020-03-14 17:10:11 +1100

    Added pipes.

--- regj-2020-unity/Assets/Scripts/Branch.cs
+++ regj-2020-unity/Assets/Scripts/Branch.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Branch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Pipe pipePrefab = null;
+
+    public int pipeCount = 4;
+
+    [SerializeField]
+    private Pipe[] pipes;
+
+    [SerializeField]
+    private bool doBuildPipes = false;
+
+    private void Awake()
     {
-        
+        BuildPipes();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ClearPipes()
     {
-        
+        foreach (Pipe pipe in pipes)
+        {
+            GameObject.Destroy(pipe.gameObject);
+        }
+        pipes = null;
+    }
+
+    public void BuildPipes()
+    {
+        ClearPipes();
+
+        pipes = new Pipe[pipeCount];
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Pipe pipe = pipes[i] = Instantiate<Pipe>(pipePrefab);
+            pipe.transform.SetParent(transform, false);
+            if (i > 0)
+            {
+                pipe.AlignWith(pipes[i - 1]);
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (doBuildPipes)
+        {
+            doBuildPipes = false;
+            BuildPipes();
+        }
     }
 }
---
+++ regj-2020-unity/Assets/Scripts/Pipe.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public float pipeRadius = 1f;
+    public int pipeSegmentCount = 10;
+
+    public float curveRadius = 4f;
+    public int curveSegmentCount = 20;
+
+    // Random Parameters
+    public float minCurveRadius = 4;
+
+    public float maxCurveRadius = 20;
+
+    public int minCurveSegmentCount = 4;
+    public int maxCurveSegmentCount = 10;
+    //
+
+    public float ringDistance = 1;
+
+    [SerializeField]
+    private float curveAngle;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    [SerializeField]
+    private bool drawGizmo = false;
+
+    [SerializeField]
+    private bool doBuildMesh = false;
+
+    private void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Pipe";
+
+        curveRadius = Random.Range(minCurveRadius, maxCurveRadius);
+        curveSegmentCount =
+            Random.Range(minCurveSegmentCount, maxCurveSegmentCount + 1);
+
+        BuildMesh();
+    }
+
+    private void OnValidate()
+    {
+        if (doBuildMesh)
+        {
+            doBuildMesh = false;
+            BuildMesh();
+        }
+    }
+
+    public void BuildMesh()
+    {
+        SetVertices();
+        SetTriangles();
+        mesh.RecalculateNormals();
+    }
+
+    public void AlignWith(Pipe pipe)
+    {
+        float relativeRotation = Random.Range(0f, 360f);
+
+        transform.SetParent(pipe.transform, false);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(0f, 0f, -pipe.curveAngle);
+        transform.Translate(0f, pipe.curveRadius, 0f);
+        transform.Rotate(relativeRotation, 0f, 0f);
+        transform.Translate(0f, -curveRadius, 0f);
+        transform.SetParent(pipe.transform.parent);
+    }
+
+    #region Mesh Generation
+
+    private void SetVertices()
+    {
+        vertices = new Vector3[pipeSegmentCount * curveSegmentCount * 4];
+
+        float uStep = ringDistance / curveRadius;
+        curveAngle = uStep * curveSegmentCount * (360f / (2f * Mathf.PI));
+        CreateFirstQuadRing(uStep);
+        int iDelta = pipeSegmentCount * 4;
+        for (int u = 2, i = iDelta; u <= curveSegmentCount; u++, i += iDelta)
+        {
+            CreateQuadRing(u * uStep, i);
+        }
+        mesh.vertices = vertices;
+    }
+
+    private void SetTriangles()
+    {
+        triangles = new int[pipeSegmentCount * curveSegmentCount * 6];
+        for (int t = 0, i = 0; t < triangles.Length; t += 6, i += 4)
+        {
+            triangles[t] = i;
+            triangles[t + 1] = triangles[t + 4] = i + 1;
+            triangles[t + 2] = triangles[t + 3] = i + 2;
+            triangles[t + 5] = i + 3;
+        }
+        mesh.triangles = triangles;
+    }
+
+    private void CreateFirstQuadRing(float u)
+    {
+        float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+
+        Vector3 vertexA = GetPointOnTorus(0f, 0f);
+        Vector3 vertexB = GetPointOnTorus(u, 0f);
+        for (int v = 1, i = 0; v <= pipeSegmentCount; v++, i += 4)
+        {
+            vertices[i] = vertexA;
+            vertices[i + 1] = vertexA = GetPointOnTorus(0f, v * vStep);
+            vertices[i + 2] = vertexB;
+            vertices[i + 3] = vertexB = GetPointOnTorus(u, v * vStep);
+        }
+    }
+
+    private void CreateQuadRing(float u, int i)
+    {
+        float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+        int ringOffset = pipeSegmentCount * 4;
+
+        Vector3 vertex = GetPointOnTorus(u, 0f);
+        for (int v = 1; v <= pipeSegmentCount; v++, i += 4)
+        {
+            vertices[i] = vertices[i - ringOffset + 2];
+            vertices[i + 1] = vertices[i - ringOffset + 3];
+            vertices[i + 2] = vertex;
+            vertices[i + 3] = vertex = GetPointOnTorus(u, v * vStep);
+        }
+    }
+
+    private Vector3 GetPointOnTorus(float u, float v)
+    {
+        Vector3 p;
+        float r = (curveRadius + pipeRadius * Mathf.Cos(v));
+        p.x = r * Mathf.Sin(u);
+        p.y = r * Mathf.Cos(u);
+        p.z = pipeRadius * Mathf.Sin(v);
+        return p;
+    }
+
+    #endregion Mesh Generation
+
+    private void OnDrawGizmos()
+    {
+        if (drawGizmo == false) return;
+
+        float uStep = (2f * Mathf.PI) / curveSegmentCount;
+        float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+
+        for (int u = 0; u < curveSegmentCount; u++)
+        {
+            for (int v = 0; v < pipeSegmentCount; v++)
+            {
+                Vector3 point = GetPointOnTorus(u * uStep, v * vStep);
+                Gizmos.color = new Color(
+                    1f,
+                    (float)v / pipeSegmentCount,
+                    (float)u / curveSegmentCount);
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+}
